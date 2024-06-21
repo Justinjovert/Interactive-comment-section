73afbdde4a83f2c32ad779f3253d6a3b41ec514a
Justinjovert <justinjoags@gmail.com>
2024-06-21 21:00:29 +0800

    added script for replying or new comment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const SESSION_ID = 4
+const SESSION_USER = 'juliusomo'
 
 
 
@@ -8,12 +8,12 @@ const cloneSection = document.getElementById('clone-this-section')
 
 // Checks user session and change accordingly
 const user_session = () => {
-    const allCommentCards = document.querySelectorAll('[data-userID]')
+    const allCommentCards = document.querySelectorAll('[data-id]')
 
-    // Changes session with value of SESSION_ID as user
+    // Changes session with value of SESSION_USER as user
     allCommentCards.forEach(commentCard => {
         // If it matches, change "reply" button to "edit" and "delete"
-        if (Number(commentCard.dataset.userid) === SESSION_ID) {
+        if (commentCard.querySelector('[data-username]').textContent === SESSION_USER) {
             const buttonReply = commentCard.querySelector('.reply')
             if (buttonReply) {
                 const thisStyle = window.getComputedStyle(buttonReply)
@@ -34,7 +34,7 @@ const user_session = () => {
 // Create a comment card
 const createCommentCard = (dataComment) => {
     const commentCard = cloneSection.content.querySelector('.comment-card').cloneNode(true)
-    commentCard.dataset.userid = dataComment.id
+    commentCard.dataset.id = dataComment.id
     commentCard.querySelector('[data-upvotes]').textContent = dataComment.score
     commentCard.querySelector('[data-user-avatar]').src = dataComment.user.image.webp
     commentCard.querySelector('[data-username]').textContent = dataComment.user.username
@@ -61,19 +61,37 @@ const createComment = (dataComment) => {
     // If reply exists
     // Create a data
     if (dataComment.replies.length >= 1) {
-        const replies = document.createElement('div')   
+        const replies = document.createElement('div')
         replies.classList.add('replies-to-parent')
         const line = document.createElement('div') // Reply line
         line.classList.add('line')                 // Interactable
-        replies.appendChild(line)   
+        replies.appendChild(line)
         dataComment.replies.forEach(reply => {
-            const replyCard = createCommentCard(reply)  
+            const replyCard = createCommentCard(reply)
             replies.appendChild(replyCard)
         })
         newCommentContainer.appendChild(replies)
     }
 }
 
+//  Count how many comments
+function countComments(data) {
+    let count = 0;
+
+    function recurse(comments) {
+        comments.forEach(comment => {
+            count++;
+            if (comment.replies && comment.replies.length > 0) {
+                recurse(comment.replies);
+            }
+        });
+    }
+
+    recurse(data.comments);
+    return count;
+}
+
+let existingIDs = 0
 // Fetch data.json on the same directory
 document.addEventListener('DOMContentLoaded', () => {
     fetch('data.json')
@@ -84,6 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return response.json()
         })
         .then(data => {
+            existingIDs = countComments(data)
             const comments = data.comments
             comments.forEach(dataComment => {
                 createComment(dataComment)
@@ -96,34 +115,114 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+// Base on data.json
+// Make new ID
+const newID = () => {
+    let new_id = existingIDs + 1
+    existingIDs = new_id
+    return new_id
+}
+
+
+
+// create object
+const createDataObj = (thisUserinput) => {
+    const commentContent = thisUserinput.querySelector("textArea").value
+    if (commentContent != '') {
+        const currentUser = thisUserinput.querySelector('.userinput-avatar')
+        const currentUsername = currentUser.dataset.userinputUsername
+        const currentUserAvatar = currentUser.querySelector('img').getAttribute('src')
+        const commentID = newID()
+
+
+        const object = {
+            "id": commentID,
+            "content": commentContent,
+            "createdAt": "1 month ago",
+            "score": 0,
+            "user": {
+                "image": {
+                    "png": "",
+                    "webp": `./${currentUserAvatar}`
+                },
+                "username": currentUsername
+            },
+            replies: []
+        }
+        console.log(object.user.image.webp)
+        const commentCard = createCommentCard(object)
+        return commentCard
+    }
+}
+
 // When user replies to an existing comment
 // Using event delegation, trace user id and create reply container
 commentSection.addEventListener('click', event => {
     // If target is reply button
-    if(event.target.classList.contains('reply')){
-        const commentParent = event.target.closest('[data-userID]')
+    if (event.target.classList.contains('reply')) {
+        const commentParent = event.target.closest('[data-id]')
         const parentComment = commentParent.closest('article')
 
-        // If there is an active reply container
-        console.log(commentParent.parentNode.querySelector('.userinput'))
-        if(!commentParent.parentNode.querySelector('.userinput')){
+        // If there isn't an active reply container
+        if (!commentParent.parentNode.querySelector('.userinput')) {
             // Create a reply container
             const replyContainer = cloneSection.content.querySelector('.userinput').cloneNode(true)
-    
-            // Get username
+
+            // Get username and ID
+            const parentID = commentParent.dataset.id
             const parentUsername = commentParent.querySelector('[data-username]').textContent
             replyContainer.getElementsByTagName('textArea')[0].value = `@${parentUsername} `
-            console.log(replyContainer.getElementsByTagName('textArea')[0].value)
-    
+            replyContainer.dataset.parentid = parentID
+
             // If it is a reply to a reply
-            if(commentParent.parentNode.classList.contains('replies-to-parent')){
+            if (commentParent.parentNode.classList.contains('replies-to-parent')) {
                 commentParent.parentNode.insertBefore(replyContainer, commentParent.nextElementSibling)
             }
-            else{
+            else {
                 parentComment.insertBefore(replyContainer, commentParent.nextElementSibling)
             }
         }
-
-        
     }
+
+    // If target is SEND button
+    // Posting a comment / reply
+    if (event.target.tagName === 'BUTTON' && event.target.parentNode.classList.contains('userinput')) {
+        if (event.target.id == 'newComment') {
+            // Extract data
+            const thisUserInput = event.target.parentNode
+            const commentContent = thisUserInput.querySelector("textArea").value
+            // If userinput is not empty
+            // Create a comment card with user comment content and data
+            // Then append before user input container
+            if (commentContent != '') {
+                const comment = createDataObj(thisUserInput)
+                const newCommentContainer = document.createElement('article')
+                newCommentContainer.classList.add('comment-container')
+                newCommentContainer.appendChild(comment)
+                commentSection.insertBefore(newCommentContainer, userinput)
+            }
+            console.log('New comment')
+        }
+        else {
+            // Look for parent comment ID, add object in reply array
+            const parentID = event.target.closest('.userinput').dataset.parentid
+            const dataID = `[data-id="${parentID}"]`
+            //const replyContainer = document.querySelector(dataID).parentNode.querySelector('.replies-to-parent')
+            const thisUserInput = event.target.closest('.userinput')
+            const parentCommentContainer = thisUserInput.closest('.comment-container')
+            let replyContainer = parentCommentContainer.querySelector('.replies-to-parent')
+            // Create reply container if it doesn't exist
+            if (replyContainer === null) {
+                parentCommentContainer.insertAdjacentHTML('beforeend', '<div class = "replies-to-parent"><div class="line"></div></div>')
+                replyContainer = parentCommentContainer.querySelector('.replies-to-parent')
+            }
+            replyContainer.appendChild(createDataObj(thisUserInput))
+            console.log('New reply')
+            thisUserInput.remove()  // Remove reply container 
+        }
+    }
+    user_session()
 })
+
+
+
